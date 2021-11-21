aa091ed37bb47d93a3ac310d1950e24419bc26b0
Mounir Bkr <93410565+MounirBkr1@users.noreply.github.com>
2021-11-21 23:52:55 +0100

    Add files via upload

---
+++ FirstServlet.java
@@ -0,0 +1,44 @@
+package com.mnr.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public FirstServlet() {
+        super();
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.setContentType("text/html"); 
+		String nom = request.getParameter("name");
+		PrintWriter out= response.getWriter();
+		out.println("<html><body>");
+		out.println("<h3>  hello "+nom+ "</h3>");
+		out.println("<li>  hello "+request.getProtocol()+"</li>");
+		out.println("<li>  hello "+request.getRemoteAddr()+"</li>");
+		out.println("</body></html>");
+		
+	
+	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
